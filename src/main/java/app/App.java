99f36739b672346fceda614620a27359378c6f32
h-Soares <hiagosoares002@gmail.com>
2023-03-28 00:30:28 -0300

    Useless comments removed.

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -9,15 +9,5 @@ public class App {
 
         //Show data
         api.getTop250Movies().forEach(System.out::println);
-
-//        TO REMEMBER: Each map contains multiple keys!
-//        Map<String, String> test = new HashMap<>();
-//        test.put("title", "TITLEtest");
-//        test.put("image", "IMAGEtest");
-//        test.put("imdbRating", "IMDBtest");
-//
-//        System.out.println(test.get("title") + "\n");
-//        System.out.println(test.get("image") + "\n");
-//        System.out.println(test.get("imdbRating"));
     }
 }
