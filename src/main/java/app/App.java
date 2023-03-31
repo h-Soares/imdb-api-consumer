f99483880a5ce41a1a7729b8703e6fe73f40b8e1
h-Soares <hiagosoares002@gmail.com>
2023-03-30 22:06:01 -0300

    Hidden key.

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -2,12 +2,19 @@ package app;
 
 import api.APIConsumer;
 import api.enums.Topic;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        //Get key
+        System.out.print("KEY: ");
+        Scanner scan = new Scanner(System.in);
+        String KEY = scan.next();
+        scan.close();
+
         //Warning: maybe API image url can change!
         //Connect to API.
-        APIConsumer api = new APIConsumer(Topic.MOST_POPULAR_TVS, "k_h163zvxx");
+        APIConsumer api = new APIConsumer(Topic.MOST_POPULAR_TVS, KEY);
 
         //Show data
         api.getContentList().forEach(System.out::println);
